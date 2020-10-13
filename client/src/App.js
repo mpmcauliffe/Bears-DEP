42902ff907fa1932c5d07191746987b94b2ebfce
mpmcauliffe <mcauliffe99@gmail.com>
2020-10-12 23:10:43 -0600

    add buttons

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,31 @@
 import React from 'react'
 import { Landing } from './pages/'
-import d3 from 'd3-geo-projection'
+import { MainController } from './components'
+
 
 
 const App = props => {
-    console.log(d3)
     return (
-        <h1>hi</h1>
+        <div>
+            {/* <Landing /> */}
+            
+            <div>
+                <img 
+                    alt='map'
+                    src={require(`./components/Map/natural_earth.svg`)} 
+                    style={{ width: '77rem', }} />
+                <img 
+                    alt='brown-bear'
+                    src={require(`./components/Map/overlays/brown.svg`)} 
+                    style={{ position: 'absolute', 
+                        zIndex:10, 
+                        left: 0, 
+                        width: '77rem', }} />
+            </div>
+
+            <MainController />
+             
+        </div>
     )
 }
 
