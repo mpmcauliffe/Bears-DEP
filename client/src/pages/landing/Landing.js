c9b2a034325784cb22f1ef33f984fcdaa2eb22c1
mpmcauliffe <mcauliffe99@gmail.com>
2020-12-12 00:15:08 -0700

    update media queries

--- client/src/pages/landing/Landing.js
+++ client/src/pages/landing/Landing.js
@@ -10,11 +10,24 @@ const LandingContainer = styled.div`
     align-items: center;
 
     img { width: 40%; }
+    img:nth-child(2) { width: 27%; }
 
-    @media (max-width: 1280px) { img { width: 50%; } }
-    @media (max-width: 980px) { img { width: 63%; } }
-    @media (max-width: 768px) { img { width: 70%; } }
-    @media (max-width: 540px) { img { width: 90%; } }
+    @media (max-width: 1280px) { 
+        img { width: 50%; } 
+        img:nth-child(2) { width: 36%; }
+    }
+    @media (max-width: 980px) { 
+        img { width: 63%; } 
+        img:nth-child(2) { width: 47%; }    
+    }
+    @media (max-width: 768px) { 
+        img { width: 70%; } 
+        img:nth-child(2) { width: 57%; }
+    }
+    @media (max-width: 540px) { 
+        img { width: 90%; } 
+        img:nth-child(2) { width: 73%; }
+    }
 `
 
 export const Landing = () => {
